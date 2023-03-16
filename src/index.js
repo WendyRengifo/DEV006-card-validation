b6d16cd50773e9e5b312c93bfa880ca01a2f434d
WendyRengifo <wendyaprendiz@hotmail.com>
2023-03-16 11:17:54 -0500

    Por confirmar

--- src/index.js
+++ src/index.js
@@ -1,34 +1,7 @@
-// import validator from './validator.js';
+import validator from './validator.js';
 
-// console.log(validator);
+console.log(validator);
 
-const formulario = document.getElementById("formulario");
-const inputs = document.querySelectorAll("#formulario input")
 
-const validarFormulario = (e) => {
-  switch (e.target.name) {
-  case "card":
-        
-    break;
-
-  case "seguridad":
-        
-    break;
-
-  case "fecha":
-        
-    break;
-  }
-    
-}
-
-inputs.forEach((input) => {
-  input.addEventListener(`keyup`, validarFormulario);
-  input.addEventListener(`blur`, validarFormulario);
-});
-
-formulario.addEventListener(`submit`, (e) => {
-  e.preventDefault();
-});
 
 
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,4 @@
 const validator = {
-  // ...
 };
 
 export default validator;
