aa1a870b36ba13c7317cb5243fed02bb9fd2a945
WendyRengifo <wendyaprendiz@hotmail.com>
2023-03-22 13:25:57 -0500

    cambios en el index se agrego un form

--- src/index.js
+++ src/index.js
@@ -4,26 +4,4 @@ console.log(validator);
 
 
 
-const crediCard = document.querySelector("[name=tarjeta]");
-const mes = document.querySelector("[name=mes]");
-const year = document.querySelector("[name=year]");
-const cvc = document.querySelector("[name=cvc]");
-
-const validadorFile = (e) => {
-  const field = e.target;
-  const filedValue = e.target.value;
-  
-  if(filedValue.length === 0){
-    field.classList.add("invalid")
-  }else{
-    field.classList.remove("invalid")
-  }
-}
-
-crediCard.addEventListener("blur", validadorFile);
-mes.addEventListener("blur", validadorFile);
-year.addEventListener("blur", validadorFile);
-cvc.addEventListener("blur", validadorFile);
-
-
 
