a97a7b689d04e6bb81f299450fd7baf2b0fe36be
WendyRengifo <wendyaprendiz@hotmail.com>
2023-03-28 19:03:49 -0500

    Se pasan las pruebas unitarias

--- src/validator.js
+++ src/validator.js
@@ -1,9 +1,36 @@
-const validator ={
-   
-    
-};
+const validator = {
+  isValid:(numeroTarjeta) =>  {
+    // Convertir la cadena de dígitos en un array de números
+    const numeros = numeroTarjeta.split('').map(Number);
 
+    // Iterar por el array de números, comenzando por el penúltimo dígito y saltando de dos en dos posiciones
+    let suma = 0;
 
+    for(let i = numeros.length -2; i >=0; i-=2){
+      let num = numeros [i] * 2;
+      if(num > 9){
+        num -= 9;
+      }
+      numeros[i] = num;
+    }
 
+    // Sumar todos los dígitos del array
+    suma = numeros.reduce((total, num) => total + num);
+
+    // Validar si la suma total es divisible por 10
+    return suma % 10 === 0;
+  },
+
+  maskify: (numeroTarjetaMascara) =>{
+    if (numeroTarjetaMascara.length <= 4) {
+      return numeroTarjetaMascara;
+    } else {
+      const ultimosCuatroDigitos = numeroTarjetaMascara.substring(numeroTarjetaMascara.length - 4);
+      const digitosEnmascarados = "#".repeat(numeroTarjetaMascara.length - 4);
+      return digitosEnmascarados + ultimosCuatroDigitos;
+    }
+  }
+
+}
 
 export default validator;
